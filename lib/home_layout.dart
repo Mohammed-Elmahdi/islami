afb71fdad09d2277c1e7881087d8e26ff4316de9
Mohammed Elmahdi <mohamedelmahdi306@gmail.com>
2022-07-24 06:46:09 +0200

    creat quran screen and bottom nav bar

--- lib/home_layout.dart
+++ lib/home_layout.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:islami_project/taps/ahadeth.dart';
+import 'package:islami_project/taps/quran/quran.dart';
+import 'package:islami_project/taps/radio.dart';
+import 'package:islami_project/taps/sebha.dart';
 
-class HomeLayout extends StatelessWidget {
+class HomeLayout extends StatefulWidget {
   static const String routeName = 'home';
 
   @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/main_pg.png',
+          height: double.infinity,
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          //to change background to main image
+          appBar: AppBar(
+            title: Text(
+              'Islami',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              // to change color
+              canvasColor: Theme.of(context).primaryColor,
+            ),
+            child: BottomNavigationBar(
+              onTap: (index) {
+                currentIndex = index;
+                setState(() {});
+              },
+              currentIndex: currentIndex,
+              items: [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                    label: 'Quran'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/ahadeth.png')),
+                    label: 'Ahadeth'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/sebha.png')),
+                    label: 'Tasbeeh'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                    label: 'Radio'),
+              ],
+            ),
+          ),
+          body: tabs[currentIndex],
+        ),
+      ],
+    );
   }
+
+  List<Widget> tabs = [
+    QuranScreen(),
+    SebhaScreen(),
+    RadioScreen(),
+    AhadethScreen(),
+  ];
 }
